2de74b6d6bb309852114796ea6c9399d10f1d1b4
moon <hoopster@airi.kr>
2021-10-12 00:16:26 +0900

    feat(GameResult.java, GameResultTest.java, ErrorCode.java) add Model for GameResult
    - 레이싱게임의 진행과 결과를 나타내는 GameResult 모델을 추가하였습니다.
    - GameResult는 자동차 콜렉션인 cars와 게임 횟수인 gameCount로 레이싱 게임을 진행합니다.
    - 게임 진행 중, 자동차의 전진을 출력할 수 있도록 문자열 리스트 moveResultList를 사용하였습니다.
    - 게임의 우승자를 문자열 winner에 저장합니다.
    - 게임 횟수를 입력 받았을 시 정수 값이 아닐 시 CarException을 발생시키도록 ErrorCode를 추가 하였습니다.

--- src/main/java/racinggame/code/ErrorCode.java
+++ src/main/java/racinggame/code/ErrorCode.java
@@ -2,7 +2,8 @@ package racinggame.code;
 
 public enum ErrorCode {
 	INVALID_NAME_RANGE_ERROR("자동차의 이름은 5자 이하만 가능합니다."),
-	INVALID_INPUT_ERROR("자동차 이름은 빈값이 포함될 수 없습니다.");
+	INVALID_INPUT_CAR_NAME_ERROR("자동차 이름은 빈값이 포함될 수 없습니다."),
+	INVALID_INPUT_GAME_COUNT_ERROR("횟수는 정수만 입력 가능합니다.");
 
 	private String message;
 
